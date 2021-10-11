32ae345ec526dbc77182917991ee81bd7b0ad65b
Evon Perez <perez.evon@gmail.com>
2021-10-11 08:16:03 -0400

    RED test api call'

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,25 +1,29 @@
-import pool from '../lib/utils/pool.js';
-import setup from '../data/setup.js';
-import request from 'supertest';
-import app from '../lib/app.js';
-import actual from'../data/actual.js';
+const pool = require('../lib/utils/pool.js') ;
+const setup = require('../data/setup.js') ;
+const request = require('supertest') ;
+const app = require('../lib/app.js') ;
+const apiCall = require('../lib/utils/helper.js');
 describe('demo routes', () => {
-  beforeEach(() => {
+  beforeAll(() => {
     return setup(pool);
   });
-
-  it('gets api repo data', () => {
-   return request(app)
-   .get('/api/repos')
-  //  .send()
-   .then(()=>{
-     expect().toEqual();
-   })
-  
-    expect().toEqual(actual);
+  //-------------------------------------------------//
+  it('gets api repo data', async () => {
+    const api_data = await apiCall();
+    const res = await request(app).get('/api/repos');
+    expect(api_data).toEqual(res);
   });
-
+   
+  //--------------------------------------------//
+  //-------------------------------------------------//
+  // it('gets api repo data', () => {
+  //   const res = request(app).get('/api/repos')
+  //   expect(res[0]).toEqual()
+ 
+ 
+  //--------------------------------------------//
   afterAll(() => {
     pool.end();
   });
+  //------------------------------------------//
 });
